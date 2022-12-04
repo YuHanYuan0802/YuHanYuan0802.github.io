d3eb3c84526dad2df335249946305bdc5ed7b544
Yu <yhanyuan2@gmail.com>
2022-12-04 18:48:50 +0800

    added js folder

---
+++ assignment/js/fragment.js
@@ -0,0 +1,9 @@
+function function1() {
+    document.getElementById("fragmentedu").style.display = "flex";
+    document.getElementById("fragmentexpe").style.display = "none";
+}
+
+function function2() {
+    document.getElementById("fragmentedu").style.display = "none";
+    document.getElementById("fragmentexpe").style.display = "flex";
+}
