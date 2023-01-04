a1f7afa12bc90caf2807aef1d6b8865e6fcba47e
YuHanYuan0802 <yuhanyuan0802@e.newera.edu.my>
2023-01-04 14:48:02 +0800

    added shortcut icon for php

--- assignment/sub.php
+++ assignment/sub.php
@@ -2,7 +2,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="phpform.css">
+    <link rel="stylesheet" href="css_folder/phpform.css">
+    <link rel="shortcut icon" href="img/y-logo.png">
     <title>Subscribe</title>
 </head>
 <body>
--- assignment/submit.php
+++ assignment/submit.php
@@ -3,6 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css_folder/phpform.css">
+    <link rel="shortcut icon" href="img/y-logo.png">
     <title>Submitted-Form</title>
 </head>
 <body>
